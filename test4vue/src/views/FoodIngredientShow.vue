7a2a7f908dd5b41da267fbb404669e5a26216313
sotiris <sot1bis@yahoo.gr>
2019-07-14 16:01:38 +0300

    lol

--- test4vue/src/views/FoodIngredientShow.vue
+++ test4vue/src/views/FoodIngredientShow.vue
@@ -7,20 +7,25 @@
     <p>Quantity: {{ FoodIngredient.quantity }} </p>
     <Button @click="deleteIt()">Delete</Button> 
     <br><br>
-    <select v-model="FoodIngredient.products">
-        <option disabled hidden :value="null">Please select one</option>
+    <select v-model="product">
         <option
           v-for="option in ProductOptions"
-          v-bind:value="option.id"
+          v-bind:value="option"
           v-bind:key="option.id"
         >{{ option.name }}</option>
       </select>
+      <BaseInput
+        label="quantity:"
+        v-model="quantity"
+        class="field"
+      />
+      <span v-if="!(isNaN(product.price * quantity))">price per FoodIngredient: {{product.price * quantity}}</span>
       <BaseButton @click="addToList()">Add to list</BaseButton>
-      <div v-if="list.length > 0">
-        <div v-for="l in list" :key="l.id">
-          {{ l }}
+      <!-- <div v-if="list.length > 0">
+        <div v-for="l in list" :key="l.name">
+          name:{{ l.name }} <br> id:{{ l.id }} <br> unit:{{ l.unitOfMeasureId}} <br> vat:{{ l.vatCategoryId }}
         </div>
-      </div>
+      </div> -->
   </div>
 </template>
 
@@ -37,7 +42,14 @@ import axios from 'axios';
         FoodIngredient:{
           result:{}
         },
-        list:[]
+        list:[],
+        product:{
+          productId:'',
+          quantity: '',
+          unitOfMeasureId: '',
+          id: ''
+        },
+        quantity:'',
       }
     },
     beforeCreate(){
@@ -45,14 +57,12 @@ import axios from 'axios';
       .get('http://localhost:21021/api/services/app/Product/GetAll?MaxResultCount=10000', {headers: {'Accept': 'application/json'}})
       .then(response =>{
         this.ProductOptions = response.data.result.items;
-        console.log(this.ProductOptions);
       })
     },
     created(){
       FoodIngredientService.getFoodIngredientById(this.id)
       .then(response => {
         this.FoodIngredient = response.data.result;
-        console.log(response.data.result);
       })
       .catch(error =>{
         console.log(error.response);
@@ -72,7 +82,20 @@ import axios from 'axios';
         })
     },
     addToList(){
-      this.list.push(this.FoodIngredient.products);
+      axios
+      .post('http://localhost:21021/api/services/app/FoodIngredient/AddProduct?foodIngredientId=' + this.id,{
+          productId: this.product.productId,
+          quantity: this.quantity,
+          unitOfMeasureId: this.product.unitOfMeasureId,
+          id: 0,
+      })
+      .then(response =>{
+        console.log(response.data);
+        console.log(this.product.id+' product id');
+        console.log(this.quantity+' quantity');
+        console.log(this.product.unitOfMeasureId+' unit id');
+      })
+      this.list.push(this.product);
       console.log(this.list);
     }
     },
